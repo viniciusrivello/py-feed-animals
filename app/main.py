8fa75adeadb832cd449a9cb84f2bc6a9aab680b3
ACER <rivelloviniciussilva@gmail.com>
2026-02-24 20:16:15 -0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-#  write your code here
+def feed_animals(animals):
+    """
+    Calcula o total de pontos de comida consumidos usando sum()
+    e uma expressão geradora conforme a diretriz nº 2.
+    """
+    return sum(animal.feed() for animal in animals)
+
+if __name__ == "__main__":
+    # Seus testes aqui
+    pass
